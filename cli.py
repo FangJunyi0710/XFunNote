ae4e8d6aa2d84fece3a2fededbbe71c5fe13da77
FangJunyi0710 <junyi.fang@qq.com>
2026-06-16 09:11:00 +0800

    重命名了少量文件。确认了架构

---
+++ cli.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# cli.py
+import sys
+import typer
+from xfun import *
+
+app = typer.Typer()
+
+@app.command()
+def init():
+	db.init()
+	# ...
+	# 其他初始化操作
+
+@app.command()
+def add(notename:str,entry:object):
+	# 向指定的某本子添加条目
+	registry.notebook(notename).add(entry)
+
+
+if __name__ == "__main__":
+	app()
