a2bb1f152328c7932495e2b31f5cbcea394adbde
FangJunyi0710 <junyi.fang@qq.com>
2026-06-18 12:06:38 +0800

    time补充时区信息

--- xfun/utils/time_utils.py
+++ xfun/utils/time_utils.py
@@ -1,6 +1,6 @@
-from datetime import datetime, timezone, tzinfo
+from datetime import datetime, timezone
 
 
 def now_str() -> str:
-    """返回当前时间字符串，包含 UTC 偏移，如 "2026-06-16 14:30:00+08:00"。"""
-    return datetime.now().isoformat(sep=" ", timespec="seconds")
+    """返回当前本地时间字符串，包含 UTC 偏移，如 "2026-06-16 14:30:00+08:00"。"""
+    return datetime.now(timezone.utc).astimezone().isoformat(sep=" ", timespec="seconds")
