0f47eaba78492907f8081c10e080dfb001f64761
FangJunyi0710 <junyi.fang@qq.com>
2026-06-18 15:13:14 +0800

    时间精确到毫秒

--- xfun/utils/time_utils.py
+++ xfun/utils/time_utils.py
@@ -3,4 +3,4 @@ from datetime import datetime, timezone
 
 def now_str() -> str:
     """返回当前本地时间字符串，包含 UTC 偏移，如 "2026-06-16 14:30:00+08:00"。"""
-    return datetime.now(timezone.utc).astimezone().isoformat(sep=" ", timespec="seconds")
+    return datetime.now(timezone.utc).astimezone().isoformat(sep=" ", timespec="milliseconds")
